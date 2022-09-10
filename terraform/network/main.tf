4b2aa83b513aceae7023f912d50c18958db32811
taki-21 <taki.it.0208.21@gmail.com>
2022-09-10 22:50:48 +0900

    add: route table for public subnet

--- terraform/network/main.tf
+++ terraform/network/main.tf
@@ -38,6 +38,12 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
+resource "aws_route" "public" {
+  route_table_id         = aws_vpc.vpc.default_route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.vpc.id
   tags = {
