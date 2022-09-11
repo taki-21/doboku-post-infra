3181fbfcb680c09bab6f03e898c410cbb5e52b64
taki-21 <taki.it.0208.21@gmail.com>
2022-09-12 00:51:46 +0900

    add: spaモジュール読み込みの記述

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,3 +2,10 @@ module "network" {
   source   = "./network"
   app_name = var.app_name
 }
+
+module "spa" {
+  source   = "./spa"
+  app_name = var.app_name
+  domain   = var.domain
+  acm_id   = module.acm.acm_id
+}
