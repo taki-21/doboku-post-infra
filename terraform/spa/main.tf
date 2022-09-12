2bc787d272623ef4dadcec24f4a9f0825ad4d485
taki-21 <taki.it.0208.21@gmail.com>
2022-09-13 06:32:46 +0900

    add: cloudfrontの設定

--- terraform/spa/main.tf
+++ terraform/spa/main.tf
@@ -1,6 +1,6 @@
-##########
+####################
 # S3
-##########
+####################
 resource "aws_s3_bucket" "this" {
   bucket = local.bucket_name
 }
@@ -17,4 +17,57 @@ resource "aws_s3_bucket_public_access_block" "this" {
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
+}
+
+####################
+# CloudFront
+####################
+resource "aws_cloudfront_distribution" "this" {
+  aliases = [var.domain]
+
+  origin {
+    domain_name = aws_s3_bucket.this.bucket_domain_name
+    origin_id   = local.s3_origin_id
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.this.cloudfront_access_identity_path
+    }
+  }
+
+  default_cache_behavior {
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = local.s3_origin_id
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  price_class         = "PriceClass_All"
+  default_root_object = "index.html"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn      = var.acm_id
+    minimum_protocol_version = "TLSv1.2_2019"
+    ssl_support_method       = "sni-only"
+  }
+}
+
+# OAIの設定
+resource "aws_cloudfront_origin_access_identity" "this" {
+  comment = var.app_name
 }
