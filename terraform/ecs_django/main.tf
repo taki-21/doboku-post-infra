ef2b5ee139c1be749627777cea5dc802fe7d618e
taki-21 <taki.it.0208.21@gmail.com>
2022-09-18 18:35:08 +0900

    add: ecs

---
+++ terraform/ecs_django/main.tf
@@ -0,0 +1,7 @@
+# ECSタスク実行ロールの作成
+module "ecs_task_execution_role" {
+  source     = "../iam_role"
+  name       = "ecs-task-execution"
+  identifier = "ecs-tasks.amazonaws.com"
+  policy     = data.aws_iam_policy_document.ecs_task_execution.json
+}
