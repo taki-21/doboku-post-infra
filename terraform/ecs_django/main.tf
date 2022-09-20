df91110c11aae501cfc1b2c902b4da1b8c390c81
taki-21 <taki.it.0208.21@gmail.com>
2022-09-20 23:59:53 +0900

    add: task

--- terraform/ecs_django/main.tf
+++ terraform/ecs_django/main.tf
@@ -5,3 +5,15 @@ module "ecs_task_execution_role" {
   identifier = "ecs-tasks.amazonaws.com"
   policy     = data.aws_iam_policy_document.ecs_task_execution.json
 }
+
+resource "aws_ecs_task_definition" "task_definition" {
+  family = "${var.app_name}-task"
+
+  cpu                      = 256
+  memory                   = 512
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+
+  container_definitions = data.template_file.container_definitions.rendered
+  execution_role_arn    = module.ecs_task_execution_role.iam_role_arn
+}
