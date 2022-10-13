ce5130c5ac18573d110d6f646880c410158a44ca
taki-21 <taki.it.0208.21@gmail.com>
2022-10-13 23:53:58 +0900

    add: output

---
+++ terraform/elb/outputs.tf
@@ -0,0 +1,11 @@
+output "http_listener_arn" {
+  value = aws_lb_listener.http.arn
+}
+
+output "https_listener_arn" {
+  value = aws_lb_listener.https.arn
+}
+
+output "alb_security_group" {
+  value = aws_security_group.this.id
+}
